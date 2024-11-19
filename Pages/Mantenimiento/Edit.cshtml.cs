95113f6de4ad953116669b6a39b71fa0479ac675
Carlos Andres Orozco Illera <carlosandresorozcoillera@gmail.com>
2024-11-19 15:19:17 -0500

    pages mantenimiento

---
+++ Pages/Mantenimiento/Edit.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FleetManager.Pages.Mantenimiento
+{
+    public class EditModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
