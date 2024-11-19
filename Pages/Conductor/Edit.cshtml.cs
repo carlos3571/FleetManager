89d0c03106468fc370df3fcf651efeb0fee42ace
Carlos Andres Orozco Illera <carlosandresorozcoillera@gmail.com>
2024-11-19 14:43:49 -0500

    editar conductor

--- Pages/Conductor/Edit.cshtml.cs
+++ Pages/Conductor/Edit.cshtml.cs
@@ -1,12 +1,64 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using FleetManager.Data;
+using FleetManager.Models;
+using System.Threading.Tasks;
 
-namespace FleetManager.Pages.Conductor
+namespace FleetManager.Pages.Conductores
 {
     public class EditModel : PageModel
     {
-        public void OnGet()
+        private readonly FleetManagerContext _context;
+
+        public EditModel(FleetManagerContext context)
         {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Conductor Conductor { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Conductor = await _context.Conductores.FindAsync(id);
+
+            if (Conductor == null)
+            {
+                return NotFound();
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var conductorExistente = await _context.Conductores.FindAsync(Conductor.IdConductor);
+
+            if (conductorExistente == null)
+            {
+                return NotFound();
+            }
+
+            conductorExistente.Nombre = Conductor.Nombre;
+            conductorExistente.Apellido = Conductor.Apellido;
+            conductorExistente.LicenciaConduccion = Conductor.LicenciaConduccion;
+            conductorExistente.Telefono = Conductor.Telefono;
+            conductorExistente.Direccion = Conductor.Direccion;
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("Index");
         }
     }
 }
+
